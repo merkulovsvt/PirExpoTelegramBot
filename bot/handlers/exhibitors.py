c760fb3f971d97bdc113d442f75b4800e92a1274
Svyatoslav Merkulov <merkulov.svttt@gmail.com>
2024-04-16 17:30:46 +0300

    fix exhibitors

--- bot/handlers/exhibitors.py
+++ bot/handlers/exhibitors.py
@@ -1,5 +1,4 @@
 from aiogram import F, Router, types
-from aiogram.enums import ParseMode
 from aiogram.fsm.context import FSMContext
 
 from bot.data.exhibitors_data import (get_exhibitor_details,
@@ -8,12 +7,11 @@ from bot.data.exhibitors_data import (get_exhibitor_details,
 from bot.keyboards.exhibitors_boards import (inline_exhibitors_details,
                                              inline_exhibitors_letter_search,
                                              inline_exhibitors_list,
-                                             inline_exhibitors_menu,
-                                             inline_exhibitors_name_search,
-                                             inline_exhibitors_search_types)
+                                             inline_exhibitors_list_by_name,
+                                             inline_exhibitors_menu)
 from bot.utils.callbackdata import ExhibitorInfo, ExhibitorSearchInfo
 from bot.utils.filters import LoggedIn
-from bot.utils.states import Exhibitors
+from bot.utils.states import Exhibitors, User
 
 router = Router()
 
@@ -33,15 +31,7 @@ async def callback_exhibitors_menu_view(callback: types.CallbackQuery):
     await callback.answer()
 
 
-# Хендлер по выводу типов поиска экспонентов через inline кнопку +
-@router.callback_query(LoggedIn(), F.data == "exhibitors_search_types")
-async def callback_exhibitors_search_type(callback: types.CallbackQuery):
-    text, keyboard = inline_exhibitors_search_types()
-    await callback.message.edit_text(text=text, reply_markup=keyboard)
-    await callback.answer()
-
-
-# Хендлер по выводу меню поиска по типу ~
+# Хендлер по выводу меню поиска по типу +
 @router.callback_query(LoggedIn(), F.data.startswith("exhibitors_searching_"))
 async def callback_exhibitors_search_type(callback: types.CallbackQuery, state: FSMContext):
     search_type = callback.data.split("_")[2]
@@ -61,16 +51,18 @@ async def callback_exhibitors_search_type(callback: types.CallbackQuery, state: 
 ignore_text = ["🛒 Заказы", "🎫 Билеты", "🗓️ Расписание", "🎉 Мероприятия", "🤝 Экспоненты"]
 
 
-# Хендлер по поиску по названию
+# Хендлер по поиску по названию +
 @router.message(Exhibitors.searching, ~F.text.lower().in_(ignore_text))
-async def exhibitors_name_search(message: types.Message):
+async def exhibitors_name_search(message: types.Message, state: FSMContext):
     user_input = message.text
+    exhibitors = get_exhibitors_list(user_input=user_input)
+    await state.set_state(User.logged_in)
 
-    text, keyboard = inline_exhibitors_name_search(user_input=user_input)
+    text, keyboard = inline_exhibitors_list_by_name(exhibitors=exhibitors, page=1, user_input=user_input)
     await message.answer(text=text, reply_markup=keyboard)
 
 
-# Хендлер по поиску через inline кнопку
+# Хендлер по поиску через inline кнопку +
 @router.callback_query(LoggedIn(), ExhibitorSearchInfo.filter())
 async def callback_exhibitors_search(callback: types.CallbackQuery, callback_data: ExhibitorSearchInfo):
     if callback_data.full:
@@ -93,7 +85,7 @@ async def callback_exhibitors_search(callback: types.CallbackQuery, callback_dat
     await callback.answer()
 
 
-# Хендлер для вывода данных экспонента
+# Хендлер для вывода данных экспонента +
 @router.callback_query(LoggedIn(), ExhibitorInfo.filter())
 async def callback_exhibitor_details(callback: types.CallbackQuery, callback_data: ExhibitorInfo):
     exhibitor_id = callback_data.exhibitor_id
@@ -106,7 +98,7 @@ async def callback_exhibitor_details(callback: types.CallbackQuery, callback_dat
     await callback.message.edit_text(text=text, reply_markup=keyboard)
 
 
-# Хендлер для inactive кнопок
+# Хендлер для inactive кнопок +
 @router.callback_query(F.data == "inactive")
 async def callback_inactive(callback: types.CallbackQuery):
     await callback.answer()
--- bot/keyboards/exhibitors_boards.py
+++ bot/keyboards/exhibitors_boards.py
@@ -11,23 +11,11 @@ def inline_exhibitors_menu():
     builder = InlineKeyboardBuilder()
     text = "Это меню экспонентов. Нажмите на кнопку для продолжения."
 
+    builder.button(text="Поиск по названию", callback_data="exhibitors_searching_name")
+    builder.button(text="Поиск по алфавиту", callback_data="exhibitors_searching_letter")
+
     builder.button(text="Все экспоненты",
                    callback_data=ExhibitorSearchInfo(full=True, letter="*", page=1, user_input="*"))
-    builder.button(text="Поиск", callback_data="exhibitors_search_types")
-
-    builder.adjust(2, 1)
-    return text, builder.as_markup()
-
-
-# Inline клавиатура для выбора типа поиска экспонентов +
-def inline_exhibitors_search_types():
-    builder = InlineKeyboardBuilder()
-    text = "Выберите тип поиска:"
-
-    builder.button(text="По названию", callback_data="exhibitors_searching_name")
-    builder.button(text="По первой букве", callback_data="exhibitors_searching_letter")
-    builder.button(text="Вернуться в меню", callback_data="exhibitors_menu")
-
     builder.adjust(2, 1)
     return text, builder.as_markup()
 
@@ -42,21 +30,51 @@ def inline_exhibitors_letter_search(letters: list):
                        callback_data=ExhibitorSearchInfo(full=False, letter=letter, page=1, user_input="*"))
     builder.adjust(5, repeat=True)
 
-    builder.row(InlineKeyboardButton(text="Отменить поиск", callback_data="exhibitors_search_types"))
+    builder.row(InlineKeyboardButton(text="Отменить поиск", callback_data="exhibitors_menu"))
 
     return text, builder.as_markup()
 
-
-# Inline клавиатура для поиска по названию
-def inline_exhibitors_name_search(user_input: str):
+# Inline клавиатура для вывода списка экспонентов по названию (старт)
+def inline_exhibitors_list_by_name(exhibitors: list, page: int, user_input: str):
     builder = InlineKeyboardBuilder()
-    text = f"Ваш запрос: {user_input}"
 
-    builder.button(text="Подтвердить",
-                   callback_data=ExhibitorSearchInfo(full=False, letter="*", page=1, user_input=user_input))
-    builder.button(text="Повторить", callback_data="exhibitors_searching_name")
-    builder.button(text="Отменить поиск", callback_data="exhibitors_search_types")
-    builder.adjust(2, 1)
+    if exhibitors:
+        text = "Экспоненты:"
+        buttons_on_page = 7
+        exhibitors_set = set()
+
+        for exhibitor in exhibitors:
+            exhibitors_set.add((exhibitor["name"], exhibitor["id"]))
+
+        pages_count = math.ceil(len(exhibitors_set) / buttons_on_page)
+
+        if page <= 0:
+            page = 1
+        elif page > pages_count:
+            page = pages_count
+
+        buttons_count = 0
+        for exhibitor in sorted(exhibitors_set)[(page - 1) * buttons_on_page:page * buttons_on_page]:
+            buttons_count += 1
+            builder.button(text=exhibitor[0],
+                           callback_data=ExhibitorInfo(exhibitor_id=exhibitor[1], full=False, letter="*", page=page,
+                                                       user_input=user_input))
+
+        builder.adjust(1)
+
+        if len(exhibitors) > buttons_on_page:
+            builder.button(text="←", callback_data=ExhibitorSearchInfo(full=False, letter="*", page=page - 1,
+                                                                       user_input=user_input) if page > 1 else "inactive")
+            builder.button(text=f"{page}/{pages_count}", callback_data="inactive")
+            builder.button(text="→", callback_data=ExhibitorSearchInfo(full=False, letter="*", page=page + 1,
+                                                                       user_input=user_input) if page < pages_count else "inactive")
+            builder.adjust(*[1 for _ in range(buttons_count)], 3)
+
+    else:
+        text = "По вашему запросу компаний не найдено"
+
+    builder.row(InlineKeyboardButton(text="Продолжить поиск", callback_data=f"exhibitors_searching_name"),
+                InlineKeyboardButton(text="Отменить поиск", callback_data="exhibitors_menu"))
 
     return text, builder.as_markup()
 
@@ -100,16 +118,16 @@ def inline_exhibitors_list(exhibitors: list, prev_callback_data: dict, list_type
             builder.button(text=f"{page}/{pages_count}", callback_data="inactive")
             builder.button(text="→", callback_data=ExhibitorSearchInfo(full=full, letter=letter, page=page + 1,
                                                                        user_input=user_input) if page < pages_count else "inactive")
-            builder.adjust(*[1 for cnt in range(buttons_count)], 3)
+            builder.adjust(*[1 for _ in range(buttons_count)], 3)
 
     else:
-        text = "По вашему запросу компаний не найдено:"
+        text = "По вашему запросу компаний не найдено"
 
     if list_type == "full":
         builder.row(InlineKeyboardButton(text="Вернуться в меню", callback_data="exhibitors_menu"))
     else:
         builder.row(InlineKeyboardButton(text="Продолжить поиск", callback_data=f"exhibitors_searching_{list_type}"),
-                    InlineKeyboardButton(text="Отменить поиск", callback_data="exhibitors_search_types"))
+                    InlineKeyboardButton(text="Отменить поиск", callback_data="exhibitors_menu"))
 
     return text, builder.as_markup()
 
@@ -117,17 +135,21 @@ def inline_exhibitors_list(exhibitors: list, prev_callback_data: dict, list_type
 # Inline клавиатура для вывода данных экспонента
 def inline_exhibitors_details(exhibitor_details: dict, exhibitors_list_data: dict):
     builder = InlineKeyboardBuilder()
-    # TODO дописать текст
-    full = exhibitors_list_data["full"]
-    letter = exhibitors_list_data["letter"]
-    page = exhibitors_list_data["page"]
-    user_input = exhibitors_list_data["user_input"]
-    text = (f"Компания {exhibitor_details['name']}\n" 
-            f"{exhibitor_details['description']}\n"
-            f"Телефон: {exhibitor_details['phone']}\n"
-            f"Почта: {exhibitor_details['email']}\n"
-            f"Сайт: {exhibitor_details['website']}\n"
-            f"Расположение {exhibitor_details['booths'][0]['booth_number']} Зал {exhibitor_details['booths'][0]['hall_number']}")
+    full = exhibitors_list_data.get("full")
+    letter = exhibitors_list_data.get("letter")
+    page = exhibitors_list_data.get("page")
+    user_input = exhibitors_list_data.get("user_input")
+
+    booths_data = ''
+    for elem in exhibitor_details['booths']:
+        booths_data += f'Зал {elem.get("hall_number")} {elem.get("booth_number")} '
+
+    text = (f"Компания: {exhibitor_details.get('name')}\n"
+            f"Описание: {exhibitor_details.get('description')}\n"
+            f"Телефон: {exhibitor_details.get('phone')}\n"
+            f"Почта: {exhibitor_details.get('email')}\n"
+            f"Сайт: {exhibitor_details.get('website')}\n"
+            f"Расположение: {booths_data}")
     builder.button(text="Вернуться к списку экспонентов",
                    callback_data=ExhibitorSearchInfo(full=full, letter=letter, page=page, user_input=user_input))
     return text, builder.as_markup()
